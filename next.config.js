7dd1b001e320e0a3e9deac429f32e499efa2f7f3
parmis069 <p.paschaei@student.fontys.nl>
2026-06-28 23:45:35 +0200

    Fix Vercel tracing root

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  outputFileTracingRoot: require("path").join(__dirname, "../../"),
   images: {
     qualities: [75, 90, 95],
     remotePatterns: [
